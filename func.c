e4f72969e4dabae4629d4a96a1cfc31a4047c456
saryunadab <saryunadabason@gmail.com>
2024-05-24 01:11:51 +0900

    Delete func.c

--- func.c
+++
@@ -1,8 +0,0 @@
-#include "header.h"
-
-void uravnenie(struct Line l) {
-    float a = (float)(l.y2 - l.y1) / (l.x2 - l.x1);
-    float b = l.y1 - a * l.x1;
-
-    printf("uravnenie pryamoy: y = %f x + %f\n", a, b);
-}
